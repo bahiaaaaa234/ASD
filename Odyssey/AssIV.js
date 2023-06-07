ec88e3cd81311559fe083d0a8482f643514143bc
bahiaaaaa234 <feninja9@gmail.com>
2023-06-07 14:04:09 -0300

    0.0.2

--- Odyssey/AssIV.js
+++ Odyssey/AssIV.js
@@ -2,6 +2,7 @@ var playButton = document.querySelector('.play');
 var closeButton = document.querySelector('.close');
 var trailer = document.querySelector('.trailer');
 var video = document.querySelector('.trailer video');
+
 playButton.addEventListener('click', function (event) {
     event.preventDefault();
     trailer.classList.add('active');
@@ -27,3 +28,42 @@ function toggleVideo() {
     var trailer = document.getElementById('trailer');
     trailer.classList.toggle('active');
 }
+
+function checkScroll() {
+    var elements = document.querySelectorAll('.scroll-transition img');
+    var windowHeight = window.innerHeight;
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+
+        if (positionFromTop - windowHeight <= 0) {
+            element.classList.add('visible');
+            element.style.animationDelay = i * 0.2 + 's'; // Adicione um atraso progressivo
+        }
+    }
+}
+
+function debounce(func, wait = 10, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+window.addEventListener('scroll', debounce(checkScroll));
+
+// Chame checkScroll uma vez para verificar se as imagens já estão visíveis na página
+checkScroll();
+
+
+//movimentação dos textos add dia 01/06
