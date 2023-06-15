28726903ed0a2c916336c4eb5a92f564337b071a
bahiaaaaa234 <feninja9@gmail.com>
2023-06-15 08:44:55 -0300

    ADD novo site

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function clickey(id) {
     console.log(id)
     switch (id) {
         case 'BlackFlag':
-        window.open("youtube.com")
+            window.open("youtube.com")
             break;
         case 'Valhalla':
             window.open("./valhalla/AssV.html")
             break
         case 'Odyssey':
-                window.open("./Odyssey/AssIV.html")
-                break
+            window.open("./Odyssey/AssIV.html")
+            break
+        case 'Origins':
+            window.open("./Origins/AssIII.html")
+            break
         default:
             break;
     }
